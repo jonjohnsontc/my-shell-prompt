70293b73e657aa59d7b1c4f3f4005077723f572b
Jon Johnson <jonjohnsontc@live.com>
2025-09-01 17:24:57 -0700

    Adding shell prompt script

---
+++ MyShellPrompt.scala
@@ -0,0 +1,38 @@
+//> using scala 3.7.1
+//> using platform native
+//> using nativeVersion 0.5.8
+
+//> using toolkit default
+
+import scala.io.AnsiColor._
+
+object Prompt:
+  
+  @main def display(): Unit = 
+    val currentDir = os.pwd
+    
+    var localDir = String()
+    if currentDir.lastOpt.isDefined then
+      localDir = currentDir.last
+    else
+      localDir = currentDir.toString
+
+    val branch = getGitBranch() 
+    if branch.isDefined then
+      println(s"${BLUE}${BOLD} On branch ${branch.get} ${RESET}")
+
+    println(s"${GREEN}${BOLD} ❧ JJ @ mac IN ${localDir} ☙ ${RESET}")
+    println(s"zsh>> ")
+  
+
+  private def getGitBranch(path: os.Path = os.pwd): Option[String] = 
+    if os.exists(path / ".git") then
+      val head = os.read(os.pwd / ".git" / "HEAD").trim
+      if head.startsWith("ref: refs/heads/") then
+        Some(head.substring("ref: refs/heads".length))
+      else
+        Some(head.take(7))  // Detached HEAD SHA
+    else
+      None
+
+
